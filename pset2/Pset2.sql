37973b031f5a7ef9e7499546c71230c49b1e9c32
Gabriel Araújo Lira <103531983+GabrielAraujo027@users.noreply.github.com>
2022-05-12 20:31:21 -0300

    Update and rename Pset2.txt to Pset2.sql

--- pset2/Pset2.sql
+++ pset2/Pset2.sql
@@ -1,5 +1,5 @@
 --Questão 01
-SELECT nome_departamento AS Departamento, CONCAT('R$ ', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial FROM funcionarios f INNER JOIN departamento d
+SELECT nome_departamento AS departamento, CONCAT('R$ ', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial FROM funcionarios f INNER JOIN departamento d
 WHERE f.numero_departamento = d.numero_departamento GROUP BY nome_departamento;
 
 
@@ -9,7 +9,7 @@ CONCAT('R$ ', CAST(AVG(salario) AS DECIMAL(10,2))) AS Média_salarial FROM funci
 
 
 --Questão 03
-SELECT nome_departamento AS Departamento, CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
+SELECT nome_departamento AS departamento, CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
 data_nascimento AS Data_de_nascimento,
 FLOOR(DATEDIFF(CURDATE(), data_nascimento)/325.25) AS Idade,
 CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário FROM funcionarios f INNER JOIN departamento d WHERE f.numero_departamento = d.numero_departamento
@@ -26,7 +26,7 @@ WHERE salario >= '35000';
 
 
 --Questão 05
-SELECT nome_departamento AS Departamento, g.primeiro_nome AS Gerente, f.primeiro_nome AS Funcionário, 
+SELECT nome_departamento AS departamento, g.primeiro_nome AS Gerente, f.primeiro_nome AS Funcionário, 
 CONCAT('R$ ', CAST((salario) AS DECIMAL(10,2))) AS Salário FROM departamento d INNER JOIN funcionarios f, 
 (SELECT primeiro_nome, cpf FROM funcionario f INNER JOIN departamento d WHERE f.cpf = d.cpf_gerente) AS g
 WHERE d.numero_departamento = f.numero_departamento AND g.cpf = d.cpf_gerente
@@ -43,14 +43,14 @@ INNER JOIN dependente dpd ON dpd.cpf_funcionario = f.cpf;
 
 
 --QUESTÃO 07
-SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, dto.nome_departamento AS Departamento,
+SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, dto.nome_departamento AS departamento,
 CONCAT('R$ ', CAST((f.salario) AS DECIMAL(10,2))) AS Salário FROM funcionarios f
 INNER JOIN departamento dto INNER JOIN dependente dpd
 WHERE dto.numero_departamento = f.numero_departamento AND f.cpf NOT IN (SELECT dpd.cpf_funcionario FROM dependente dpd);
 
 
 --Questão 08
-SELECT d.nome_departamento AS Departamento, p.nome_projeto AS Projeto,
+SELECT d.nome_departamento AS departamento, p.nome_projeto AS Projeto,
 CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, t.horas AS Horas
 FROM funcionarios f INNER JOIN departamento d INNER JOIN projeto p INNER JOIN trabalha_em t WHERE d.numero_departamento = f.numero_departamento
 AND p.numero_projeto = t.numero_projeto AND f.cpf = t.cpf_funcionario
@@ -58,14 +58,14 @@ ORDER BY p.numero_projeto;
 
 
 --Questão 09
-SELECT d.nome_departamento AS Departamento, p.nome_projeto AS Projeto, SUM(t.horas) AS Total_de_horas 
+SELECT d.nome_departamento AS departamento, p.nome_projeto AS Projeto, SUM(t.horas) AS Total_de_horas 
 FROM departamento d INNER JOIN projeto p INNER JOIN trabalha_em t WHERE d.numero_departamento = p.numero_departamento
 AND p.numero_projeto = t.numero_projeto
 GROUP BY p.nome_projeto;
 
 
 --Questão 10
-SELECT d.nome_departamento AS Departamento, CONCAT('R$ ', CAST(AVG(f.salario) AS DECIMAL(10,2))) AS Média_salarial
+SELECT d.nome_departamento AS departamento, CONCAT('R$ ', CAST(AVG(f.salario) AS DECIMAL(10,2))) AS Média_salarial
 FROM departamento d INNER JOIN funcionarios f WHERE d.numero_departamento = f.numero_departamento
 GROUP BY d.nome_departamento;
 
@@ -77,7 +77,7 @@ WHERE f.cpf = t.cpf_funcionario AND p.numero_projeto = t.numero_projeto GROUP BY
 
 
 --QUESTÃO 12
-SELECT d.nome_departamento AS Departamento, p.nome_projeto AS Projeto,
+SELECT d.nome_departamento AS departamento, p.nome_projeto AS Projeto,
 CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo, t.horas AS Horas
 FROM funcionarios f INNER JOIN departamento d INNER JOIN projeto p INNER JOIN trabalha_em t
 WHERE f.cpf = t.cpf_funcionario AND p.numero_projeto = t.numero_projeto AND (t.horas = 0 OR t.horas = NULL) GROUP BY f.primeiro_nome;
@@ -96,17 +96,17 @@ FLOOR(DATEDIFF(CURDATE(), d.data_nascimento)/365.25) AS Idade FROM dependente d 
 
 
 --QUESTÃO 14
-SELECT d.nome_departamento AS Departamento, COUNT(f.numero_departamento) AS Número_funcionários FROM funcionarios f INNER JOIN departamento d
+SELECT d.nome_departamento AS departamento, COUNT(f.numero_departamento) AS Número_funcionários FROM funcionarios f INNER JOIN departamento d
 WHERE f.numero_departamento = d.numero_departamento GROUP BY d.nome_departamento;
 
 
 --Questão 15
 SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
-d.nome_departamento AS Departamento, 
+d.nome_departamento AS departamento, 
 p.nome_projeto AS Projeto FROM departamento d INNER JOIN projeto p INNER JOIN trabalha_em t INNER JOIN funcionarios f 
 WHERE d.numero_departamento = f.numero_departamento AND p.numero_projeto = t.numero_projeto AND t.cpf_funcionario = f.cpf
 UNION
 SELECT DISTINCT CONCAT(f.primeiro_nome, ' ', f.nome_meio, ' ', f.ultimo_nome) AS Nome_completo,
-d.nome_departamento AS Departamento, 
+d.nome_departamento AS departamento, 
 'Sem projeto' AS Projeto FROM departamento d INNER JOIN projeto p INNER JOIN trabalha_em t INNER JOIN funcionarios f 
 WHERE d.numero_departamento = f.numero_departamento AND p.numero_projeto = t.numero_projeto AND (f.cpf NOT IN (SELECT t.cpf_funcionario FROM trabalha_em t));
